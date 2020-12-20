3ef7389e8b8768049e712f632b2f61a13db04b4d
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-12-20 14:55:54 +0100

    poprawiona wersja dziala

--- main.ts
+++ main.ts
@@ -1,97 +1,240 @@
 namespace SpriteKind {
     export const Bacteria = SpriteKind.create()
 }
+function doCheckSprite (num: number) {
+    if (num >= 0 && num <= wx * wy - 1) {
+        if (list[num].z == 1 || list[num].z == 3) {
+            return 1
+        } else {
+            return 0
+        }
+    } else {
+        return -1
+    }
+}
+function doGenerateBlinker () {
+    list[45].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[46].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[47].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[45].z = 1
+    list[46].z = 1
+    list[47].z = 1
+}
 function doCallculateCell (num: number) {
     friends = 0
-    if (num % wx > 0) {
-        if (list[num - 1].z == 1) {
-            friends += 1
-        }
+    if (num % wx > 0 && doCheckSprite(num - 1) == 1) {
+        friends += 1
     }
-    if (num % wx < wx - 1) {
-        if (list[num + 1].z == 1) {
-            friends += 1
-        }
+    if (num % wx < wx - 1 && doCheckSprite(num + 1) == 1) {
+        friends += 1
     }
-    if (num >= wx) {
-        if (list[num - wx].z == 1) {
-            friends += 1
-        }
+    if (num >= wx && doCheckSprite(num - wx) == 1) {
+        friends += 1
     }
-    if (num <= wx * wy - 1 - wx) {
-        if (list[num + wx].z == 1) {
-            friends += 1
-        }
+    if (num < (wy - 1) * wx && doCheckSprite(num + wx) == 1) {
+        friends += 1
     }
-    if (num % wx > 0 && num >= wx) {
-        if (list[num - (wx + 1)].z == 1) {
-            friends += 1
-        }
+    if (num % wx > 0 && num >= wx && doCheckSprite(num - (wx + 1)) == 1) {
+        friends += 1
     }
-    if (num % wx < wx - 1 && num >= wx) {
-        if (list[num - (wx - 1)].z == 1) {
-            friends += 1
-        }
+    if (num >= wx && num % wx < wx - 1 && doCheckSprite(num - (wx - 1)) == 1) {
+        friends += 1
     }
-    if (num <= wx * wy - 1 - (wx - 1) && num % wx > 0) {
-        if (list[num + (wx - 1)].z == 1) {
-            friends += 1
-        }
+    if (num < (wy - 1) * wx && num % wx > 0 && doCheckSprite(num + (wx - 1)) == 1) {
+        friends += 1
     }
-    if (num <= wx * wy - 1 - (wx + 1) && num % wx < wx - 1) {
-        if (list[num + (wx + 1)].z == 1) {
-            friends += 1
-        }
+    if (num < (wy - 1) * wx && num % wx < wx - 1 && doCheckSprite(num + (wx + 1)) == 1) {
+        friends += 1
     }
     return friends
+}
+function doGenerateAligator () {
+    list[45].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[46].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[47].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[48].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[49].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[50].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[64].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[71].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[83].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[92].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[104].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[111].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[125].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[126].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[127].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[128].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[129].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[130].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[45].z = 1
+    list[46].z = 1
+    list[47].z = 1
+    list[48].z = 1
+    list[49].z = 1
+    list[50].z = 1
+    list[64].z = 1
+    list[71].z = 1
+    list[83].z = 1
+    list[92].z = 1
+    list[104].z = 1
+    list[111].z = 1
+    list[125].z = 1
+    list[125].z = 1
+    list[126].z = 1
+    list[127].z = 1
+    list[128].z = 1
+    list[129].z = 1
+    list[130].z = 1
+}
+function doGenerate1 () {
+    doGenerateAligator()
+}
+function doGenerateFrog () {
+    list[45].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[46].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[64].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[87].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[105].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[106].setImage(img`
+        7 7 
+        7 7 
+        `)
+    list[45].z = 1
+    list[46].z = 1
+    list[64].z = 1
+    list[87].z = 1
+    list[105].z = 1
+    list[106].z = 1
 }
 function doLive () {
     for (let index = 0; index <= wx * wy - 1; index++) {
         howmany = doCallculateCell(index)
-        if (list[index].z == 0 && howmany == 3) {
+        z = list[index].z
+        // list[index].setImage(img`
+        // b b
+        // b b
+        // `)
+        // list[index].setImage(img`
+        // 2 2
+        // 2 2
+        // `)
+        // list[index].setImage(img`
+        // 9 9
+        // 9 9
+        // `)
+        // list[index].setImage(img`
+        // f f
+        // f f
+        // `)
+        if (z == 0 && howmany == 3) {
             list[index].z = 2
-            list[index].setImage(img`
-                b b 
-                b b 
-                `)
-        } else if (list[index].z == 1 && howmany != 2 && howmany != 3) {
+        } else if (z == 1 && howmany != 2 && howmany != 3) {
             list[index].z = 3
-            list[index].setImage(img`
-                2 2 
-                2 2 
-                `)
-        } else if (list[index].z == 1 && (howmany == 2 || howmany == 3)) {
-            list[index].setImage(img`
-                9 9 
-                9 9 
-                `)
-        } else if (list[index].z == 0 && howmany != 3) {
-            list[index].setImage(img`
-                f f 
-                f f 
-                `)
+        } else if (z == 1 && (howmany == 2 || howmany == 3)) {
+        	
+        } else if (z == 0 && howmany != 3) {
+        	
         }
     }
-    pause(1000)
     for (let index2 = 0; index2 <= wx * wy - 1; index2++) {
-        if (list[index2].z == 2) {
+        z = list[index2].z
+        if (z == 2) {
             list[index2].z = 1
             list[index2].setImage(img`
                 7 7 
                 7 7 
                 `)
-        } else if (list[index2].z == 3) {
+        } else if (z == 3) {
             list[index2].z = 0
             list[index2].setImage(img`
                 d d 
                 d d 
                 `)
-        } else if (list[index2].z == 0) {
+        } else if (z == 0) {
             list[index2].setImage(img`
                 d d 
                 d d 
                 `)
-        } else if (list[index2].z == 1) {
+        } else if (z == 1) {
             list[index2].setImage(img`
                 7 7 
                 7 7 
@@ -101,6 +244,7 @@ function doLive () {
         }
     }
 }
+let z = 0
 let howmany = 0
 let friends = 0
 let mySprite: Sprite = null
@@ -110,11 +254,11 @@ let list: Sprite[] = []
 list = []
 let dx = 2
 let dy = 2
-wy = 20
-wx = 20
+wy = 80
+wx = 60
 info.setScore(0)
 for (let index3 = 0; index3 <= wx * wy - 1; index3++) {
-    if (Math.percentChance(50)) {
+    if (Math.percentChance(16)) {
         mySprite = sprites.create(img`
             7 7 
             7 7 
